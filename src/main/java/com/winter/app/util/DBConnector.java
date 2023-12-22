4c89b90a74fa6d3b7a9cc38b5d55cc3a0646271c
Nohnon <arark057@gmail.com>
2023-12-22 12:48:29 +0900

    12/22 step1

--- src/main/java/com/winter/app/util/DBConnector.java
+++ src/main/java/com/winter/app/util/DBConnector.java
@@ -2,12 +2,14 @@ package com.winter.app.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 public class DBConnector {
 	//1. IP:PORT
 	//2. ID,PW
 	public void  getConnector() {
-		//디비버 에딧 커켓션 그대로
+		//디비버 우클릭 에딧 커켓션 그대로
 		String user = "user01";
 		String password = "user01";
 		String url = "jdbc:oracle:thin:@43.201.36.31:1521:xe";
@@ -20,7 +22,28 @@ public class DBConnector {
 			
 			//2.DB에 연결
 			Connection con = DriverManager.getConnection(url, user, password);//이정보로 로그인하고 오겠다.실패하면 null
-			System.out.println(con);//null아니면 성공
+			System.out.println(con);//null아니면 db에 접속 성공, db쿼리 결과 문자열로 받음
+			
+			//3.Sql문 생성
+			String sql = "SELECT * FROM REGIONS"; //디비버에 쳐서 되는지 확인하고 집어넣기!
+			
+			//4.SQL문 미리 전송
+			PreparedStatement st = con.prepareStatement(sql);
+			
+			//5.
+			
+			//6.최종 전송 및 결과 처리(db:결과물은 ResultSet에 보관. 자바도 같음)
+			ResultSet rs = st.executeQuery();//최종적으로 쿼리 실행해
+			//row한줄씩 읽는데 다음을 읽어라는 메서드. 내용없으면(null) false
+			while (rs.next()){
+				//한줄ㄹ 읽은걸 rs에 담음. rs = 1, Europe
+				int n = rs.getInt("REGION_ID");//컬럼명. 이걸로 쓰자 타입때문에
+//				int n = rs.getInt(1);//인덱스 번호. 컬럼명 너무길때 근데 별칭쓰면됨..
+				String name = rs.getString("REGION_NAME");
+//				String name = rs.getString(2);
+				System.out.println(n+" : "+name);
+				System.out.println("----------------------------");
+			}
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
